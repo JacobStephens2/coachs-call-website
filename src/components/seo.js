dcef6146dca6fb80608b4e33a2a5f165170244cb
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-24 16:11:51 -0400

    fix header

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,94 +1,15 @@
-/**
- * Seo component that queries for data with
- *  Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import * as React from "react"
-
-import PropTypes from "prop-types"
 
 import { Helmet } from "react-helmet"
 
-import { graphql } from "gatsby"
-import { StaticQuery } from "gatsby"
-
-function Seo({ description, lang, meta, title }) {
-
+const Seo = (props) => {
   return (
-    <StaticQuery
-      query={graphql`
-        query {
-          site {
-            siteMetadata {
-              title
-              description
-              author
-            }
-          }
-        }
-      `}
-      render={data => {
-        <Helmet
-          htmlAttributes={{
-            lang,
-          }}
-          title={data.siteMetadata?.title}
-          titleTemplate={data.siteMetadata?.title ? `%s | ${data.siteMetadata?.title}` : null}
-          meta={[
-            {
-              name: `description`,
-              content: data.sitemetaData?.description,
-            },
-            {
-              property: `og:title`,
-              content: data.site.siteMetadatatitle,
-            },
-            {
-              property: `og:description`,
-              content: data.site.siteMetadata.description,
-            },
-            {
-              property: `og:type`,
-              content: `website`,
-            },
-            {
-              name: `twitter:card`,
-              content: `summary`,
-            },
-            {
-              name: `twitter:creator`,
-              content: data.site.siteMetadata?.author || ``,
-            },
-            {
-              name: `twitter:title`,
-              content: data.site.siteMetadatatitle,
-            },
-            {
-              name: `twitter:description`,
-              content: data.site.siteMetadata.description,
-            },
-            ].concat(meta)}
-          >
+        <Helmet>
+          <title>{props.title} | Coach's Call</title>
+          <script src="https://kit.fontawesome.com/5ebbfa01b2.js" crossorigin="anonymous"></script>
           <link rel="stylesheet" href="https://use.typekit.net/fot1kfm.css"></link>
         </Helmet>
-      }}
-    />
   )
-}
-
-Seo.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-Seo.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default Seo
