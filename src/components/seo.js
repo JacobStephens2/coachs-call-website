e2ec53b61a02e27b4fad0bd1a67f96ea94094272
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-09-24 12:00:04 -0400

    test if StaticQuery component replacement
    replacement of useStaticQuery react hook to test if that would fix error, at least now one fewer error message to click through

--- src/components/seo.js
+++ src/components/seo.js
@@ -12,71 +12,70 @@ import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 
 import { graphql } from "gatsby"
-import { useStaticQuery } from "gatsby"
+import { StaticQuery } from "gatsby"
 
 function Seo({ description, lang, meta, title }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-          }
-        }
-      }
-    `
-  )
-
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
 
   return (
-    <Helmet
-      htmlAttributes={{
-        lang,
+    <StaticQuery
+      query={graphql`
+        query {
+          site {
+            siteMetadata {
+              title
+              description
+              author
+            }
+          }
+        }
+      `}
+      render={data => {
+        <Helmet
+          htmlAttributes={{
+            lang,
+          }}
+          title={data.siteMetadata?.title}
+          titleTemplate={data.siteMetadata.defaultTitle ? `%s | ${data.siteMetadata.defaultTitle}` : null}
+          meta={[
+            {
+              name: `description`,
+              content: data.sitemetaData.description,
+            },
+            {
+              property: `og:title`,
+              content: data.site.siteMetadatatitle,
+            },
+            {
+              property: `og:description`,
+              content: data.site.siteMetadata.description,
+            },
+            {
+              property: `og:type`,
+              content: `website`,
+            },
+            {
+              name: `twitter:card`,
+              content: `summary`,
+            },
+            {
+              name: `twitter:creator`,
+              content: data.site.siteMetadata?.author || ``,
+            },
+            {
+              name: `twitter:title`,
+              content: data.site.siteMetadatatitle,
+            },
+            {
+              name: `twitter:description`,
+              content: data.site.siteMetadata.description,
+            },
+            ].concat(meta)}
+          >
+          <script src="https://kit.fontawesome.com/5ebbfa01b2.js" crossorigin="anonymous"></script>
+          <link rel="stylesheet" href="https://use.typekit.net/fot1kfm.css"></link>
+        </Helmet>
       }}
-      title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:title`,
-          content: title,
-        },
-        {
-          property: `og:description`,
-          content: metaDescription,
-        },
-        {
-          property: `og:type`,
-          content: `website`,
-        },
-        {
-          name: `twitter:card`,
-          content: `summary`,
-        },
-        {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.author || ``,
-        },
-        {
-          name: `twitter:title`,
-          content: title,
-        },
-        {
-          name: `twitter:description`,
-          content: metaDescription,
-        },
-      ].concat(meta)}
-    >
-      <script src="https://kit.fontawesome.com/5ebbfa01b2.js" crossorigin="anonymous"></script>
-      <link rel="stylesheet" href="https://use.typekit.net/fot1kfm.css"></link>
-    </Helmet>
+    />
   )
 }
 
