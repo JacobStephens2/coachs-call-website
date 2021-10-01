14f0fa8cab241d9c94dcec58487117f69dfc0530
Jacob Stephens <53905376+JacobStephens2@users.noreply.github.com>
2021-10-01 18:28:57 -0400

    add analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     PRESERVE_WEBPACK_CACHE: true,
     FAST_DEV: true,
     PARALLEL_SOURCING: true,
-    LMDB_STORE: true
+    LMDB_STORE: true,
   },
   plugins: [
     {
@@ -26,6 +26,26 @@ module.exports = {
           perPage: 20, // currently set to 100
           requestConcurrency: 5, // currently set to 15
           previewRequestConcurrency: 2, // currently set to 5
+        },
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9FWTXPCKKJ", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
         },
       },
     },
@@ -63,13 +83,13 @@ module.exports = {
           placeholder: `blurred`,
         },
       },
-    },  
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-css-customs`,
     {
       resolve: `gatsby-plugin-postcss`,
       options: {
-        postCssPlugins: [require(`postcss-preset-env`)({ stage: 0})],
+        postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })],
       },
     },
     `gatsby-plugin-gatsby-cloud`,
