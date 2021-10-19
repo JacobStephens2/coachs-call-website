b08467d94ddd7e6a833171ac4e9c55704596b38e
Jacob Stephens <jacob@stewardgoods.com>
2021-10-19 11:31:03 -0400

    remove wordpress source given not being used

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,20 +16,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-source-wordpress`,
-      options: {
-        url: `https://content.coachscall.org/graphql`,
-        develop: {
-          hardCacheMediaFiles: true,
-        },
-        schema: {
-          perPage: 20, // currently set to 100
-          requestConcurrency: 5, // currently set to 15
-          previewRequestConcurrency: 2, // currently set to 5
-        },
-      },
-    },
-    {
       resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
