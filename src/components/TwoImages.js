324d11c8de2d44bda5fa40fc0e882406514cb7ea
Jacob Stephens <jacob@stewardgoods.com>
2021-10-20 16:33:16 -0400

    remove introRow crest and stack images on work pg

--- src/components/TwoImages.js
+++ src/components/TwoImages.js
@@ -1,44 +1,33 @@
-import * as React from 'react'
+import * as React from "react"
 
 import { StaticImage } from "gatsby-plugin-image"
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
-const TwoImages = (props) => {
-	if (props.image === '') {
-		return (
-			''
-		);
-	} else {
-		return (
-			<span>
-				<Row>
-					<Col>
-					<StaticImage
-						className="drop-shadow"
-						src="../images/WorkImage1.jpg"
-						alt="The Coaching Pyramid"
-						/>
-					</Col>
-					<Col>
-					<StaticImage
-						className="drop-shadow"
-						src="../images/WorkImage2.jpg"
-						alt="Joy and Performance slide"
-					/>
-					</Col>
-				</Row>
-			</span>
-		);
-	}
+const TwoImages = props => {
+  if (props.image === "") {
+    return ""
+  } else {
+    return (
+      <div className="about-photos-background">
+        <StaticImage
+          className="drop-shadow"
+          src="../images/WorkImage1.jpg"
+          alt="The Coaching Pyramid"
+        />
+        <StaticImage
+          className="drop-shadow"
+          src="../images/WorkImage2.jpg"
+          alt="Joy and Performance slide"
+        />
+      </div>
+    )
+  }
 }
 
 TwoImages.defaultProps = {
-	image: {},
-	imageAlt: {},
-	imageTwo: {},
-	imageAltTwo: {}
+  image: {},
+  imageAlt: {},
+  imageTwo: {},
+  imageAltTwo: {},
 }
 
-export default TwoImages;
+export default TwoImages
