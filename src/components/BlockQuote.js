8792bd7202c8d10354801be7d5d98d61644f95a1
Jacob Stephens <jacob@stewardgoods.com>
2021-10-25 15:50:20 -0400

    update BlockQuote component

--- src/components/BlockQuote.js
+++ src/components/BlockQuote.js
@@ -1,41 +1,40 @@
-import * as React from 'react'
+import * as React from "react"
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Container from 'react-bootstrap/Container';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
 
-const BlockQuote = (props) => {
-	if (props.quotationMark === "no") {
-		return (
-			<section className="blue-section">
-			<Container>
-				<Row>
-					<Col className="blockquote-column">
-						<blockquote>{props.quote}”</blockquote>
-						<cite>{props.source}</cite>
-					</Col>
-				</Row>
-			</Container>
-		</section>	
-		);
-	} else {
-		return (
-			<section className="blue-section">
-			<Container>
-				<Row>
-					<Col sm={12} md={2} lg={2} xl={2} className="large-quotation-mark">
-					“
-					</Col>
-					<Col sm={12} md={10} lg={8} xl={7} className="blockquote-column">
-						<blockquote>{props.quote}”</blockquote>
-						<cite>{props.source}</cite>
-					</Col>
-				</Row>
-			</Container>
-		</section>	
-		)
-	}
-
+const BlockQuote = props => {
+  if (props.quotationMark === "no") {
+    return (
+      <section className="blue-section">
+        <Container>
+          <Row>
+            <Col className="blockquote-column">
+              <blockquote>{props.quote}”</blockquote>
+              <cite>{props.source}</cite>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    )
+  } else {
+    return (
+      <section className="blue-section">
+        <Container>
+          <Row>
+            <Col sm={12} md={2} lg={2} xl={2} className="large-quotation-mark">
+              “
+            </Col>
+            <Col sm={12} md={10} lg={8} xl={7} className="blockquote-column">
+              <blockquote>{props.quote}”</blockquote>
+              <cite>{props.source}</cite>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    )
+  }
 }
 
 export default BlockQuote
