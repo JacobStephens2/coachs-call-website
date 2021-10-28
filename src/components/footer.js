312f348e19f5777a2566df8fb41761c3b3e3570f
Jacob Stephens <jacob@stewardgoods.com>
2021-10-28 16:14:46 -0400

    make footer align with left line one col

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,23 +4,25 @@ import Container from "react-bootstrap/Container"
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <Container>
-        <div>
-          © {new Date().getFullYear()}
-          {` `}
-          <a
-            rel="noreferrer"
-            href="https://www.15eastcreative.com"
-            target="_blank"
-          >
-            Coach's Call
-          </a>
-          &ensp;|&ensp; All rights reserved. &ensp;|&ensp;
-          <a href="/privacy-policy">Privacy&nbsp;Policy</a>
-        </div>
-      </Container>
-    </footer>
+    <section className="one-col-section footer-section">
+      <footer className="footer">
+        <Container>
+          <div>
+            © {new Date().getFullYear()}
+            {` `}
+            <a
+              rel="noreferrer"
+              href="https://www.15eastcreative.com"
+              target="_blank"
+            >
+              Coach's Call
+            </a>
+            &ensp;|&ensp; All rights reserved. &ensp;|&ensp;
+            <a href="/privacy-policy">Privacy&nbsp;Policy</a>
+          </div>
+        </Container>
+      </footer>
+    </section>
   )
 }
 
