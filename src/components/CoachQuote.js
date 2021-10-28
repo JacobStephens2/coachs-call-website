886f46a34e1db5a150a99a0c8e923f3f4102dbb1
Jacob Stephens <jacob@stewardgoods.com>
2021-10-28 14:56:22 -0400

    change quote mark to svg

--- src/components/CoachQuote.js
+++ src/components/CoachQuote.js
@@ -2,11 +2,13 @@ import * as React from "react"
 
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
+import QuoteMarks from "../../svg/quote-marks.svg"
 
 const CoachQuote = props => {
   return (
     <section className="coachquote-section one-col-section">
       <Container>
+        <QuoteMarks />
         <Col sm={12} md={10} lg={8} xl={7} className="coachquote-column">
           <blockquote>{props.quote}”</blockquote>
           <cite>{props.source}</cite>
