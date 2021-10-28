0c0137703d1161937ab996e9b1f32151a1a89016
Jacob Stephens <jacob@stewardgoods.com>
2021-10-28 15:33:51 -0400

    align mentoring section with sections below

--- src/components/Mentoring.js
+++ src/components/Mentoring.js
@@ -5,35 +5,28 @@ import { Link } from "gatsby"
 import Button from "react-bootstrap/Button"
 import Container from "react-bootstrap/Container"
 import Col from "react-bootstrap/Col"
-import Row from "react-bootstrap/Row"
 
 import ListItemNoDivider from "./ListItemNoDivider"
 
 const Mentoring = props => {
   return (
-    <section id={props.id} className={props.sectionClass}>
+    <section id={props.id} className="one-col-section">
       <Container>
-        <Row gx={20}>
-          <Col className="sems-and-workshops" lg={7} xxl={6}>
-            <h2>{props.heading}</h2>
-            <p className="subhead">{props.subheading}</p>
-            <p>
-              <strong>{props.bold}</strong> {props.description}
-            </p>
-            <ListItemNoDivider
-              bold={props.boldList}
-              description={props.descriptionList}
-              boldTwo={props.boldTwo}
-              descriptionTwo={props.descriptionTwo}
-              boldThree={props.boldThree}
-              descriptionThree={props.descriptionThree}
-            />
-            <Link to={props.link}>
-              <Button className="section-button">{props.buttonText}</Button>
-            </Link>
-          </Col>
-          <Col lg={5}>{props.children}</Col>
-        </Row>
+        <Col className="sems-and-workshops" lg={7} xxl={6}>
+          <h2>{props.heading}</h2>
+          <p className="subhead">{props.subheading}</p>
+          <ListItemNoDivider
+            bold={props.boldList}
+            description={props.descriptionList}
+            boldTwo={props.boldTwo}
+            descriptionTwo={props.descriptionTwo}
+            boldThree={props.boldThree}
+            descriptionThree={props.descriptionThree}
+          />
+          <Link to={props.link}>
+            <Button className="section-button">{props.buttonText}</Button>
+          </Link>
+        </Col>
       </Container>
     </section>
   )
