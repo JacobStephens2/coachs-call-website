c7b11afcc062c12501ee51fb9eeaa2db60b56da4
Jacob Stephens <jacob@stewardgoods.com>
2021-11-11 15:41:20 -0500

    move seminarimages into worksection and make mentoring child component

--- src/components/Mentoring.js
+++ src/components/Mentoring.js
@@ -3,8 +3,6 @@ import * as React from "react"
 import { Link } from "gatsby"
 
 import Button from "react-bootstrap/Button"
-import Container from "react-bootstrap/Container"
-import Col from "react-bootstrap/Col"
 
 import ListItemNoDivider from "./ListItemNoDivider"
 
@@ -12,24 +10,20 @@ import "./MentoringStyle.css"
 
 const Mentoring = props => {
   return (
-    <section id="mentoring" className="one-col-section">
-      <Container>
-        <Col lg={7} xxl={7}>
-          <h2>{props.heading}</h2>
-          <p className="subhead">{props.subheading}</p>
-          <ListItemNoDivider
-            bold={props.boldList}
-            description={props.descriptionList}
-            boldTwo={props.boldTwo}
-            descriptionTwo={props.descriptionTwo}
-            boldThree={props.boldThree}
-            descriptionThree={props.descriptionThree}
-          />
-          <Link to={props.link}>
-            <Button className="section-button">{props.buttonText}</Button>
-          </Link>
-        </Col>
-      </Container>
+    <section id="mentoring">
+      <h2>{props.heading}</h2>
+      <p className="subhead">{props.subheading}</p>
+      <ListItemNoDivider
+        bold={props.boldList}
+        description={props.descriptionList}
+        boldTwo={props.boldTwo}
+        descriptionTwo={props.descriptionTwo}
+        boldThree={props.boldThree}
+        descriptionThree={props.descriptionThree}
+      />
+      <Link to={props.link}>
+        <Button className="section-button">{props.buttonText}</Button>
+      </Link>
     </section>
   )
 }
--- src/pages/work.js
+++ src/pages/work.js
@@ -25,8 +25,6 @@ const WorkPage = ({ data }) => {
       </IntroRow>
 
       <WorkSection
-        id="seminars-and-workshops"
-        heading="Professional Development Seminars and&nbsp;Workshops"
         subheading="exploring the orthodoxy of sport and how we pursue 
 					god’s high calling to&nbsp;coach."
         link="/contact"
@@ -39,21 +37,20 @@ const WorkPage = ({ data }) => {
         descriptionThree="to use and implement into sport specific&nbsp;coaching"
         citation="John Levis"
         quote="I am created to glorify God by obeying His call through coaching and my primary responsibility is to create the 'beautiful game' as an act of worship to&nbsp;Him."
-        subClass="sems-and-workshops"
-      />
-
-      <Mentoring
-        heading="One-on-One Mentoring and Small Group Sessions"
-        subheading="Pursuing excellence"
-        buttonText="LEARN MORE"
-        link="/contact"
-        boldList="Engaging coaches"
-        descriptionList="where they are in their journey and helping them develop more fully their view of what God requires of the call to&nbsp;coaches"
-        boldTwo="Connecting coaches"
-        descriptionTwo="in practical ways to pursue excellence and see the pursuit as&nbsp;worship"
-        boldThree="Helping coaches"
-        descriptionThree="develop strategies for culture building, program development, recruiting, and day to day&nbsp;excellence"
-      />
+      >
+        <Mentoring
+          heading="One-on-One Mentoring and Small Group Sessions"
+          subheading="Pursuing excellence"
+          buttonText="LEARN MORE"
+          link="/contact"
+          boldList="Engaging coaches"
+          descriptionList="where they are in their journey and helping them develop more fully their view of what God requires of the call to&nbsp;coaches"
+          boldTwo="Connecting coaches"
+          descriptionTwo="in practical ways to pursue excellence and see the pursuit as&nbsp;worship"
+          boldThree="Helping coaches"
+          descriptionThree="develop strategies for culture building, program development, recruiting, and day to day&nbsp;excellence"
+        />
+      </WorkSection>
 
       <CoachQuote
         quote="Perpetual growth is a massive part of becoming who we want to be; as an individual, as a team, or even as a program. Proper growth often is produced not by adding but by uncovering. Working with John pushes me to dig, to uncover where my identity truly is. Not just my identity but where I want the identity of my program to&nbsp;be."
