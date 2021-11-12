a2ea9f0ab297c0648097b191bcfb3ef8828a3230
Jacob Stephens <jacob@stewardgoods.com>
2021-11-12 15:34:35 -0500

    restyle let's connect component

--- src/components/LetsConnect.js
+++ src/components/LetsConnect.js
@@ -12,30 +12,30 @@ const LetsConnect = () => {
     <section className="one-col-section">
       <Container className="lets-connect">
         <h2>Let’s Connect</h2>
-        <p>
+
+        <p className="lets-connect-quote">
           “As iron sharpens iron, so one person sharpens another.”{" "}
           <cite className="connect-cite">Proverbs 27:1</cite>
         </p>
-        <Col>
-          <span className="contact-text">
-            <p>
-              <a href="tel:484.574.1444">
-                <i className="fas fa-phone-alt"></i>
-                &ensp; 484.574.1444
-              </a>
-            </p>
-            <p>
-              <a href="mailto:john@coachscall.org">
-                <i className="far fa-envelope"></i>
-                &ensp; john@coachscall.org
-              </a>
-            </p>
-          </span>
-        </Col>
 
         <Link to="/contact">
           <Button>CONTACT JOHN</Button>
         </Link>
+
+        <span className="contact-text">
+          <p>
+            <a href="mailto:john@coachscall.org">
+              <i className="far fa-envelope"></i>
+              &ensp; john@coachscall.org
+            </a>
+          </p>
+          <p>
+            <a href="tel:484.574.1444">
+              <i className="fas fa-phone-alt"></i>
+              &ensp; 484.574.1444
+            </a>
+          </p>
+        </span>
       </Container>
     </section>
   )
