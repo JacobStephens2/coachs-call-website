1c6235aa9470268059d96381f6f3ebec97651696
Jacob Stephens <jacob@stewardgoods.com>
2021-11-15 15:42:18 -0500

    straight align bullet text on work page

--- src/components/ListItemNoDivider.js
+++ src/components/ListItemNoDivider.js
@@ -1,25 +1,25 @@
-import * as React from 'react'
+import * as React from "react"
 
-const ListItemNoDivider = (props) => {
-	return (
-		<div>
-			<li>
-				<b>{props.bold}</b>
-				<span> </span>
-				{props.description}
-			</li>
-			<li>
-				<b>{props.boldTwo}</b>
-				<span> </span>
-				{props.descriptionTwo}
-			</li>
-			<li>
-				<b>{props.boldThree}</b>
-				<span> </span>
-				{props.descriptionThree}
-			</li>
-		</div>
-	)
+const ListItemNoDivider = props => {
+  return (
+    <ul>
+      <li>
+        <b>{props.bold}</b>
+        <span> </span>
+        {props.description}
+      </li>
+      <li>
+        <b>{props.boldTwo}</b>
+        <span> </span>
+        {props.descriptionTwo}
+      </li>
+      <li>
+        <b>{props.boldThree}</b>
+        <span> </span>
+        {props.descriptionThree}
+      </li>
+    </ul>
+  )
 }
 
 export default ListItemNoDivider
